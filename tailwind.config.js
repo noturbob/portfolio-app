abe27e3c813bf0b0e9738a38d3fef36eb91ab11d
noturbob <bobbyanthenrao@gmail.com>
2025-09-30 22:42:24 +0530

    Add missing Vercel/Tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all your component files
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
